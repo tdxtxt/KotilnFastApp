c455c3ebc567478720db6f46688e4290d3f1bfe6
鬼脚七 <gjq@juexiaotime.com>
2021-12-17 10:32:11 +0800

    stateLayout

--- libCommon/src/main/java/com/baselib/ui/activity/IView.kt
+++ libCommon/src/main/java/com/baselib/ui/activity/IView.kt
@@ -1,14 +1,22 @@
 package com.baselib.ui.activity
 
+import com.baselib.rx.transformer.ProgressTransformer
+import com.baselib.rx.transformer.UIThreadTransformer
 import com.baselib.ui.dialog.child.ProgressDialog
+import com.trello.rxlifecycle3.LifecycleTransformer
 
 interface IView {
     fun getProgressBar(): ProgressDialog?
     fun hideProgressBar()
     fun showProgressBar()
+    fun showProgressBar(desc: String, isCancel: Boolean)
 
-    fun showLoadingView()
-    fun showContentView()
-    fun showEmptyView()
-    fun showErrorView(e: Throwable)
+    fun <T> bindLifecycle(): LifecycleTransformer<T>
+    fun <T> bindUIThread(): UIThreadTransformer<T>
+    fun <T> bindProgress(): ProgressTransformer<T>
+    fun <T> bindProgress(bindDialog: Boolean): ProgressTransformer<T>
+//    fun showLoadingView()
+//    fun showContentView()
+//    fun showEmptyView()
+//    fun showErrorView(e: Throwable)
 }
