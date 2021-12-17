2c658cc4237d9a673002c51db7bcd902aaee76f5
tangdexiang <tangdexiang@ccqtgb.com>
2021-12-17 21:55:11 +0800

    UI

--- app/src/main/java/com/fastdev/ui/dialog/SourceFilterDialog.kt
+++ app/src/main/java/com/fastdev/ui/dialog/SourceFilterDialog.kt
@@ -31,31 +31,39 @@ class SourceFilterDialog constructor(activity: FragmentActivity) : BottomBaseDia
         recyclerDong = findViewById(R.id.recyclerDong)
         recyclerFoor = findViewById(R.id.recyclerFoor)
         recyclerRoom = findViewById(R.id.recyclerRoom)
-        val roomList1 = mutableListOf(Place(1101, "101房间"), Place(1102, "102房间"), Place(1103, "103")
-                , Place(1104, "104房间"), Place(1105, "105房间"))
+        val roomList1 = mutableListOf(Place(1101, "1栋1楼101房间"), Place(1102, "1栋1楼102房间"), Place(1103, "1栋1楼103房间")
+                , Place(1104, "1栋1楼104房间"), Place(1105, "1栋1楼105房间"))
 
-        val roomList2 = mutableListOf(Place(1201, "201房间"), Place(1202, "202房间"), Place(1203, "203")
-                , Place(1204, "204房间"), Place(1205, "205房间"))
+        val roomList2 = mutableListOf(Place(1201, "1栋2楼201房间"), Place(1202, "1栋2楼202房间"), Place(1203, "1栋2楼203房间")
+                , Place(1204, "204房间"), Place(1205, "1栋2楼205房间"))
 
-        val roomList3 = mutableListOf(Place(2101, "101房间"), Place(2102, "102房间"), Place(2103, "103")
-                , Place(2104, "104房间"), Place(2105, "105房间"))
+        val roomList3 = mutableListOf(Place(2101, "2栋1楼101房间"), Place(2102, "2栋1楼102房间"), Place(2103, "2栋1楼103房间")
+                , Place(2104, "2栋1楼104房间"), Place(2105, "2栋1楼105房间"))
 
-        val roomList4 = mutableListOf(Place(2201, "201房间"), Place(2202, "202房间"), Place(2203, "203")
-                , Place(2204, "204房间"), Place(2205, "205房间"))
+        val roomList4 = mutableListOf(Place(2201, "2栋2楼201房间"), Place(2202, "2栋2楼202房间"), Place(2203, "2栋2楼203房间")
+                , Place(2204, "204房间"), Place(2205, "2栋2楼205房间"))
 
-        val foorList1 = mutableListOf(Place(11, "1楼", roomList1), Place(12, "2楼", roomList2))
+        val foorList1 = mutableListOf(Place(11, "1栋1楼", roomList1), Place(12, "1栋2楼", roomList2))
 
-        val foorList2 = mutableListOf(Place(21, "1楼", roomList3), Place(22, "2楼", roomList4))
+        val foorList2 = mutableListOf(Place(21, "2栋1楼", roomList3), Place(22, "2栋2楼", roomList4))
 
-        val dongList = mutableListOf(Place(1, "1栋", foorList1), Place(2, "2栋", foorList2))
+        val foorList3 = mutableListOf(Place(31, "3栋1楼"), Place(32, "3栋2楼"))
+
+        val dongList = mutableListOf(Place(1, "1栋", foorList1), Place(2, "2栋", foorList2), Place(1, "3栋", foorList3))
 
         adapterDong = object : BaseQuickAdapter<Place, BaseViewHolder>(R.layout.item_dong){
             override fun convert(holder: BaseViewHolder, item: Place) {
                 holder.setText(R.id.tv_name, item.name)
+                holder.itemView.isSelected = item.checked
             }
         }.apply {
             setOnItemClickListener { adapter, view, position ->
+                data.forEachIndexed { index, place ->
+                    data[index].checked = index == position
+                }
+                notifyDataSetChanged()
                 adapterFoor?.setNewInstance(getItem(position).childs)
+                adapterRoom?.setNewInstance(null)
             }
 
             recyclerDong?.adapter = this
