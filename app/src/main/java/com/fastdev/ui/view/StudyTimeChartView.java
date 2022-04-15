95fe4605ced2b688dd36a7db1eb3115e4d347b71
鬼脚七 <gjq@juexiaotime.com>
2022-04-15 18:44:01 +0800

    画图

--- app/src/main/java/com/fastdev/ui/view/StudyTimeChartView.java
+++ app/src/main/java/com/fastdev/ui/view/StudyTimeChartView.java
@@ -170,7 +170,7 @@ public class StudyTimeChartView extends View {
 
 
 
-        canvas.rotate(-(180 - loseAngle / 2),0f,0f);
+        canvas.rotate(-(180 - loseAngle / 2f),0f,0f);
 
         float  y = length;
         y = - y;
